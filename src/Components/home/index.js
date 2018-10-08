2ae8b16a5c6827b6c106e4b935b25ff322f339c7
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-07 23:21:34 -0500

    starts Featured component for Home

---
+++ src/Components/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Featured from "./featured";
+
+const Home = () => {
+  return (
+    <div className="bck_blue">
+      <h1>Home</h1>
+      <Featured />
+    </div>
+  );
+};
+
+export default Home;
