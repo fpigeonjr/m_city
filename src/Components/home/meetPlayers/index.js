1cfe4dd52a9fe84f2ea532ff7a1d74a15e02d199
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-11 22:23:55 -0500

    adds HomeCards component with reveal animation.

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -1,67 +1,76 @@
 import React, { Component } from "react";
 import Stripes from "../../../Resources/images/stripes.png";
 import { Tag } from "../../UI/Misc";
+import { Reveal } from "react-reveal";
+import HomeCards from "./HomeCards";
 
 class MeetPlayers extends Component {
-  state = {};
+  state = { show: false };
   render() {
     return (
-      <div
-        className="home_meetplayers"
-        style={{ background: `#ffffff url(${Stripes})` }}
+      <Reveal
+        fraction={0.7}
+        onReveal={() => {
+          this.setState({ show: true });
+        }}
       >
-        <div className="container">
-          <div className="home_meetplayers_wrapper">
-            <div className="home_card_wrapper">
-              <h1>CARD</h1>
-            </div>
-            <div className="home_text_wrapper">
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  color="#ffffff"
-                  size="100px"
-                  add={{ display: "inline-block", marginBottom: "20px" }}
-                >
-                  Meet
-                </Tag>
-                <Tag
-                  bck="#0e1731"
-                  color="#ffffff"
-                  size="100px"
-                  add={{ display: "inline-block", marginBottom: "20px" }}
-                >
-                  The
-                </Tag>
-                <Tag
-                  bck="#0e1731"
-                  color="#ffffff"
-                  size="100px"
-                  add={{ display: "inline-block", marginBottom: "20px" }}
-                >
-                  Players
-                </Tag>
+        <div
+          className="home_meetplayers"
+          style={{ background: `#ffffff url(${Stripes})` }}
+        >
+          <div className="container">
+            <div className="home_meetplayers_wrapper">
+              <div className="home_card_wrapper">
+                <HomeCards show={this.state.show} />
+              </div>
+              <div className="home_text_wrapper">
                 <div>
                   <Tag
-                    bck="#ffffff"
-                    color="#0e1731"
-                    size="27px"
-                    link
-                    linkTo="team"
-                    add={{
-                      display: "inline-block",
-                      marginBottom: "27px",
-                      border: "1px solid #0e1731"
-                    }}
+                    bck="#0e1731"
+                    color="#ffffff"
+                    size="100px"
+                    add={{ display: "inline-block", marginBottom: "20px" }}
                   >
-                    Meet them here
+                    Meet
                   </Tag>
+                  <Tag
+                    bck="#0e1731"
+                    color="#ffffff"
+                    size="100px"
+                    add={{ display: "inline-block", marginBottom: "20px" }}
+                  >
+                    The
+                  </Tag>
+                  <Tag
+                    bck="#0e1731"
+                    color="#ffffff"
+                    size="100px"
+                    add={{ display: "inline-block", marginBottom: "20px" }}
+                  >
+                    Players
+                  </Tag>
+                  <div>
+                    <Tag
+                      bck="#ffffff"
+                      color="#0e1731"
+                      size="27px"
+                      link
+                      linkTo="team"
+                      add={{
+                        display: "inline-block",
+                        marginBottom: "27px",
+                        border: "1px solid #0e1731"
+                      }}
+                    >
+                      Meet them here
+                    </Tag>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Reveal>
     );
   }
 }
