5bcb7af92485bac9332b2dc2f252499d6f3d15e2
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-14 18:49:41 -0500

    starts registration email form on homepage. adds reusable FormField component.

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import { Fade } from "react-reveal";
+import FormField from "./../../UI/FormField";
 
-const Enroll = () => {
-  return <div>Enroll</div>;
-};
+class Enroll extends Component {
+  state = {
+    formError: false,
+    formSuccess: "",
+    formData: {
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placeholder: "Enter your email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ""
+      }
+    }
+  };
+
+  handleSubmit = () => {
+    console.log("form submitted");
+  };
+
+  render() {
+    return (
+      <Fade>
+        <div className="enroll_wrapper">
+          <form onSubmit={event => this.handleSubmit(event)}>
+            <div className="enroll_title">Enter your email</div>
+            <div className="enroll_input">
+              <FormField id={'email'} formData={this.state.formData.email} />
+            </div>
+          </form>
+        </div>
+      </Fade>
+    );
+  }
+}
 
 export default Enroll;
